1573ca211750ba7be5875571a9631891880a14e2
Victor Grechi <victor_grechi@hotmail.com>
2020-08-03 11:59:37 -0300

    Resolving env files path.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 import * as dotenv from "dotenv";
+import * as path from "path";
 
-dotenv.config();
 dotenv.config({  
-    path: process.env.NODE_ENV === "test" ? `${__dirname}/../../.env.test` : `${__dirname}/../../.env`
-});
+    path: process.env.NODE_ENV === "test" ? path.resolve(`${__dirname}/../../.env.test`) : path.resolve(`${__dirname}/../../.env`)
+}); 
 
 export const PROJECT = process.env.PROJECT;
